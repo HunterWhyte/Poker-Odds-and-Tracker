3a6c8466aa355f179f6acf9613104cf3fff7eb18
hunterwhyte <Whiteone#1>
2020-04-27 23:47:18 -0600

    results GUI complete

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter.ttk import *
 import tkinter.font as tkfont
 import opens
+import results
+import tkinter.scrolledtext as scrolledtext
 
 window = Tk()
 
@@ -9,10 +11,11 @@ window.title("poker")
 
 allhands = ['aa', 'aks', 'aqs', 'ajs', 'ats', 'a9s', 'a8s', 'a7s', 'a6s', 'a5s', 'a4s', 'a3s', 'a2s', 'ako', 'kk', 'kqs', 'kjs', 'kts', 'k9s', 'k8s', 'k7s', 'k6s', 'k5s', 'k4s', 'k3s', 'k2s', 'aqo', 'kqo', 'qq', 'qjs', 'qts', 'q9s', 'q8s', 'q7s', 'q6s', 'q5s', 'q4s', 'q3s', 'q2s', 'ajo', 'kjo', 'qjo', 'jj', 'jts', 'j9s', 'j8s', 'j7s', 'j6s', 'j5s', 'j4s', 'j3s', 'j2s', 'ato', 'kto', 'qto', 'jto', 'tt', 't9s', 't8s', 't7s', 't6s', 't5s', 't4s', 't3s', 't2s', 'a9o', 'k9o', 'q9o', 'j9o', 't9o', '99', '98s', '97s', '96s', '95s', '94s', '93s', '92s', 'a8o', 'k8o', 'q8o', 'j8o', 't8o', '98o', '88', '87s', '86s', '85s', '84s', '83s', '82s', 'a7o', 'k7o', 'q7o', 'j7o', 't7o', '97o', '87o', '77', '76s', '75s', '74s', '73s', '72s', 'a6o', 'k6o', 'q6o', 'j6o', 't6o', '96o', '86o', '76o', '66', '65s', '64s', '63s', '62s', 'a5o', 'k5o', 'q5o', 'j5o', 't5o', '95o', '85o', '75o', '65o', '55', '54s', '53s', '52s', 'a4o', 'k4o', 'q4o', 'j4o', 't4o', '94o', '84o', '74o', '64o', '54o', '44', '43s', '42s', 'a3o', 'k3o', 'q3o', 'j3o', 't3o', '93o', '83o', '73o', '63o', '53o', '43o', '33', '32s', 'a2o', 'k2o', 'q2o', 'j2o', 't2o', '92o', '82o', '72o', '62o', '52o', '42o', '32o', '22']
 
+# hand lookup
+
 fontsize = 20
 lg = tkfont.Font(family="Lucida Grande", size=fontsize) # use for widgets that don't follow default font size
 tkfont.nametofont('TkDefaultFont').configure(size=fontsize) # change default font size
-
 
 position = IntVar()
 position.set(1)
@@ -36,13 +39,13 @@ def lookuphand():
     resultlabel.configure(text = result)
 
 handentry = Entry(window, width = 14,font = lg)
-handentry.grid(column=0, row=3)
+handentry.grid(column=0, columnspan=3, row=3)
 
-resultlabel = Label(window, text="enter hand", font=lg)
-resultlabel.grid(column=0, row=1)
+resultlabel = Label(window, text="preflop hand", font=lg)
+resultlabel.grid(column=0, columnspan=3, row=1)
 
-button1 = Button(window, text='Look up', command=lookuphand)
-button1.grid(column=0, row=4)
+lookupbutton = Button(window, text='Look up', command=lookuphand)
+lookupbutton.grid(column=0, columnspan=3, row=4)
 
 rad1 = Radiobutton(window,text='BU open  ', value=1, variable=position)
 
@@ -54,14 +57,149 @@ rad4 = Radiobutton(window,text='Nash push', value=4, variable=position)
 
 rad5 = Radiobutton(window,text='Nash call', value=5, variable=position)
 
-rad1.grid(column=1, row=0)
+rad1.grid(column=3, columnspan=3, row=0)
 
-rad2.grid(column=1, row=1)
+rad2.grid(column=3, columnspan=3, row=1)
 
-rad3.grid(column=1, row=2)
+rad3.grid(column=3, columnspan=3, row=2)
 
-rad4.grid(column=1, row=3)
+rad4.grid(column=3, columnspan=3, row=3)
 
-rad5.grid(column=1, row=4)
+rad5.grid(column=3, columnspan=3, row=4)
+
+# win tracking
+buyinvalue = StringVar()
+buyinvalue.set("")
+prizepoolvalue = StringVar()
+prizepoolvalue.set("")
+emptylabel = Label(window, text="", font=lg)
+emptylabel.grid(column = 0, row = 5)
+emptylabel2 = Label(window, text="", font=lg)
+emptylabel2.grid(column = 0, row = 6)
+
+winloss = IntVar()
+def saveresults():
+    buyin = buyinentry.get()
+    prizepool = prizepoolentry.get()
+    try:
+        buyin = float(buyin)
+        buyinvalue.set("")
+    except:
+        buyinvalue.set("inval")
+        prizepoolvalue.set("inval")
+        return
+    try:
+        prizepool = float(prizepool)
+        prizepoolvalue.set("")
+    except:
+        prizepoolvalue.set("inval")
+        buyinvalue.set("inval")
+        return
+    wl = winloss.get()
+
+    results.add_results(buyin, prizepool, wl)
+
+
+buyinlabel = Label(window, text="buyin$:", font=lg)
+prizepoollabel = Label(window, text="prize$:", font=lg)
+buyinentry = Entry(window, width = 5,font = lg, justify = "right", textvariable = buyinvalue)
+prizepoolentry = Entry(window, width = 5,font = lg, justify = "right", textvariable = prizepoolvalue)
+
+buyinlabel.grid(column = 0, row = 7, sticky = "w")
+prizepoollabel.grid(column = 0, row = 8, sticky = "w")
+buyinentry.grid(column=1, row=7)
+prizepoolentry.grid(column=1, row=8)
+
+win = Radiobutton(window,text='W', value=1, variable=winloss)
+loss = Radiobutton(window,text='L', value=0, variable=winloss)
+enterresultbutton = Button(window, text='enter result', command=saveresults)
+win.grid(column=3, row=7)
+loss.grid(column=4, row=7)
+enterresultbutton.grid(column = 3, columnspan = 2, row = 8)
+
+def list_results():
+    win = Toplevel()
+    win.wm_title("resultslist")
+
+    txt = scrolledtext.ScrolledText(win, width=20, height=40, undo = True)
+
+    txt['font'] = ('consolas', '12')
+    txt.pack(expand=True, fill='both')
+    txt.grid(row=0, column=0)
+    txt.insert(1.0, results.get_results())
+    b = Button(win, text="Okay", command=win.destroy)
+    b.grid(row=1, column=0)
+listresultbutton = Button(window, text='list results', command=list_results)
+listresultbutton.grid(column = 0, columnspan = 2, row = 9, pady = 20)
+def graph_results():
+    buyin = buyinentry.get()
+    if buyin == "all" or buyin == "":
+        results.display_results(0)
+        buyinvalue.set("")
+        return
+    try:
+        buyin = float(buyin)
+        buyinvalue.set("")
+    except:
+        buyinvalue.set("inval")
+        return
+    results.display_results(str(buyin))
+    return
+graphresultbutton = Button(window, text='graph results', command=graph_results)
+graphresultbutton.grid(column = 3, columnspan = 2, row = 9)
+
+# odds calculator
+
+# initialize card string variables
+hand1card1 = StringVar()
+hand1card2 = StringVar()
+hand2card1 = StringVar()
+hand2card2 = StringVar()
+
+flopcard1 = StringVar()
+flopcard2 = StringVar()
+flopcard3 = StringVar()
+turncard  = StringVar()
+rivercard = StringVar()
+
+oddscalculatorlabel = Label(window, text="odds:", font=lg)
+oddscalculatorlabel.grid(column = 7, row = 1, sticky = "w")
+
+hand1oddslabel = Label(window, text="0.00%", font=lg)
+hand1oddslabel.grid(column = 8, row = 4, columnspan = 4, sticky = "w")
+
+hand1card1entry = Entry(window, width = 2,font = lg, justify = "left", textvariable = hand1card1)
+hand1card2entry = Entry(window, width = 2,font = lg, justify = "left", textvariable = hand1card2)
+
+hand1card1entry.grid(column = 8, row = 3, sticky = "w")
+hand1card2entry.grid(column = 9, row = 3, sticky = "w")
+
+flopcard1entry = Entry(window, width = 2,font = lg, justify = "left", textvariable = flopcard1)
+flopcard2entry = Entry(window, width = 2,font = lg, justify = "left", textvariable = flopcard2)
+flopcard3entry = Entry(window, width = 2,font = lg, justify = "left", textvariable = flopcard3)
+turncardentry = Entry(window, width = 2,font = lg, justify = "left", textvariable = turncard)
+rivercardentry = Entry(window, width = 2,font = lg, justify = "left", textvariable = rivercard)
+
+
+flopcard1entry.grid(column = 10, row = 5, sticky = "w")
+flopcard2entry.grid(column = 11, row = 5, sticky = "w")
+flopcard3entry.grid(column = 12, row = 5, sticky = "w")
+turncardentry.grid(column = 13, row = 5, sticky = "w")
+rivercardentry.grid(column = 14, row = 5, sticky = "w")
+
+hand2card1entry = Entry(window, width = 2,font = lg, justify = "left", textvariable = hand2card1)
+hand2card2entry = Entry(window, width = 2,font = lg, justify = "left", textvariable = hand2card2)
+
+hand2card1entry.grid(column = 15, row = 3, sticky = "w")
+hand2card2entry.grid(column = 16, row = 3, sticky = "w")
+
+hand2oddslabel = Label(window, text="0.00%", font=lg)
+hand2oddslabel.grid(column = 15, row = 4, columnspan = 4, sticky = "w")
+
+def calculate_odds():
+    return
+
+calculateoddsbutton = Button(window, text='calculate', command=calculate_odds)
+calculateoddsbutton.grid(column = 10, row = 7, columnspan = 5)
 
 window.mainloop()
--- results.py
+++ results.py
@@ -1,24 +1,30 @@
 import matplotlib.pyplot as plt
 
 def add_results(buyin, prize, result):
+    f = open("results.txt", "a")
+    f.write(str(buyin) + " " + str(prize) + " " + str(result) + "\n")
+    f.close()
     return
-
+def get_results():
+    f = open("results.txt")
+    resultstext = f.read()
+    f.close()
+    return resultstext
 def display_results(stakes):
     """
     display chip ev, bankroll, win% for given stake
     """
     f = open("results.txt")
     games = f.read().rstrip().split("\n")
+    f.close()
     for i in range(len(games)):
         games[i] = games[i].split(" ")
     results = []
     for i in range(len(games)):
-        if stakes == "all":
+        if stakes == 0:
             results.append(games[i])
-        if games[i][0] == stakes:
+        elif float(games[i][0]) == float(stakes):
             results.append(games[i])
-
-    print(results)
     bankroll = []
     chips = []
     winloss = 0
@@ -37,14 +43,19 @@ def display_results(stakes):
         chips.append(totalchips)
         # win/loss
         winloss += int(results[i][2])
-    winlossper = winloss/len(results)
-    print(bankroll)
-    print(chips)
-    print(winlossper*100)
+    if winloss>0:
+        winlossper = winloss/len(results)
+    else:
+        fig, ax = plt.subplots()
+        plt.suptitle("no data available for these stakes")
+        plt.show()
+        return
 
     fig, ax1 = plt.subplots()
-
+    fig.subplots_adjust(top=0.2)
     color = 'tab:red'
+    if stakes == 0:
+        stakes = "all"
     plt.suptitle(stakes + " Spin & Go Results || win% ="+ "{:8.4f}".format((winlossper*100)) + "%")
     ax1.set_xlabel('tournaments')
     ax1.set_ylabel('winnings USD', color=color)
@@ -60,5 +71,4 @@ def display_results(stakes):
     ax2.set_ylim([0, max(chips) + 2000])
     fig.tight_layout()  # otherwise the right y-label is slightly clipped
     plt.show()
-
-display_results("0.25")
+    return
